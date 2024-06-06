a97b000c373066d95974ae0242a7a685ab2766d7
Zixuan Chen <remch183@outlook.com>
2024-06-07 01:29:45 +0800

    feat: expose more functions and types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-export { LoroSyncPlugin } from "./sync-plugin";
+export {
+  LoroSyncPlugin,
+  loroSyncPluginKey,
+  LoroSyncPluginProps,
+  LoroSyncPluginState,
+} from "./sync-plugin";
 export type { LoroDocType } from "./lib";
+export {
+  createNodeFromLoroObj,
+  updateLoroOnPmChange,
+  ROOT_DOC_KEY,
+  NODE_NAME_KEY,
+  CHILDREN_KEY,
+  ATTRIBUTES_KEY,
+  LoroNodeMapping,
+} from "./lib";
 export { LoroCursorPlugin } from "./cursor-plugin";
 export { CursorAwareness } from "./awareness";
-export { LoroUndoPlugin, undo, redo, canUndo, canRedo } from "./undo-plugin";
+export {
+  LoroUndoPlugin,
+  loroUndoPluginKey,
+  LoroUndoPluginProps,
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+} from "./undo-plugin";
