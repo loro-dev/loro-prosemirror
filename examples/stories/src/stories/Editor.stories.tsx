657d9523c6b9e5b9b752215fdfa88545948d23fd
raymonddaikon <raymonddai@live.com>
2024-10-30 17:12:43 -0700

    Update storybook example and add BC example.

--- examples/stories/src/stories/Editor.stories.tsx
+++ examples/stories/src/stories/Editor.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta } from "@storybook/react";
 
-import { CursorAwareness, LoroDocType } from "loro-prosemirror";
-import { useEffect, useRef } from "react";
 import { Editor } from "./Editor";
-import { LoroDoc } from "loro-crdt";
+import { LoroDoc, VersionVector } from "loro-crdt";
+import { useEffect, useRef, useState } from "react";
+import { CursorAwareness, LoroDocType } from "loro-prosemirror";
 
 const meta = {
   title: "Editor/Basic",
@@ -27,6 +27,53 @@ export const Basic = () => {
   );
 };
 
+type UpdateType = 'ephemeral' | 'awareness' | 'crdt'
+type UpdateMessage = {
+  type: 'update'
+  updateType: UpdateType
+  payload: Uint8Array
+}
+
+function parseMessage(data: Uint8Array): UpdateMessage {
+  const messageType = data[0]
+  switch (messageType) {
+    case 1: {
+      // Update
+      const updateType = (() => {
+        switch (data[1]) {
+          case 0:
+            return 'ephemeral'
+          case 1:
+            return 'awareness'
+          case 2:
+            return 'crdt'
+          default:
+            throw new Error(`Unknown update type: ${data[1]}`)
+        }
+      })()
+      return {
+        type: 'update',
+        updateType,
+        payload: data.slice(2),
+      }
+    }
+    default:
+      throw new Error(`Unknown message type: ${messageType}`)
+  }
+}
+
+function encodeUpdateMessage(
+  updateType: UpdateType,
+  payload: Uint8Array,
+): Uint8Array {
+  const message = new Uint8Array(2 + payload.length)
+  message[0] = 1
+  message[1] =
+    updateType === 'ephemeral' ? 0 : updateType === 'awareness' ? 1 : 2
+  message.set(payload, 2)
+  return message
+}
+
 export const Sync = () => {
   const loroARef = useRef<LoroDocType>(new LoroDoc());
   const idA = loroARef.current.peerIdStr;
@@ -38,14 +85,14 @@ export const Sync = () => {
     loroARef.current.subscribe((event) => {
       if (event.by === "local") {
         loroBRef.current.import(
-          loroARef.current.exportFrom(loroBRef.current.oplogVersion()),
+          loroARef.current.export({ mode: 'update', from: loroBRef.current.oplogVersion() }),
         );
       }
     });
     loroBRef.current.subscribe((event) => {
       if (event.by === "local") {
         loroARef.current.import(
-          loroBRef.current.exportFrom(loroARef.current.oplogVersion()),
+          loroBRef.current.export({ mode: 'update', from: loroARef.current.oplogVersion() }),
         );
       }
     });
@@ -70,3 +117,49 @@ export const Sync = () => {
     </div>
   );
 };
+
+export const BroadcastChannel = () =>{
+  // @ts-expect-error
+  const bcA = useRef<BroadcastChannel>(new BroadcastChannel(`A`))
+  const loroARef = useRef<LoroDocType>(new LoroDoc());
+  const idA = loroARef.current.peerIdStr;
+  const awarenessA = useRef<CursorAwareness>(new CursorAwareness(idA));
+  const [lastStateA, setLastStateA] = useState<VersionVector | undefined>();
+  useEffect(() => {
+    bcA.current.onmessage = (event) => {
+      const parsedMessage = parseMessage(event.data)
+      if (parsedMessage.type === 'update') {
+        // Handle different update types
+        switch (parsedMessage.updateType) {
+          case 'ephemeral':
+            break
+          case 'awareness':
+            break
+          case 'crdt':
+            loroARef.current.import(parsedMessage.payload)
+            loroARef.current.commit({origin: 'sys:bc-update'})
+            break
+        }
+      }
+    }
+    loroARef.current.subscribe((event) => {
+      if (event.by === "local") {
+        bcA.current.postMessage(encodeUpdateMessage('crdt', loroARef.current.export({ mode: 'update', from: lastStateA})))
+        setLastStateA(loroARef.current.version())
+      }
+    });
+    awarenessA.current.addListener((_state, origin) => {
+      if (origin === "local") {
+        bcA.current.postMessage(encodeUpdateMessage('awareness', awarenessA.current.encode([idA])))
+      }
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <Editor loro={loroARef.current} awareness={awarenessA.current}  />
+    </div>
+  );
+}
