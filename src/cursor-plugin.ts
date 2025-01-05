47bf9921b0dd952042afee81421f5e724df94c17
Zixuan Chen <remch183@outlook.com>
2025-01-05 18:46:54 +0800

    chore: remove incorrect error messages (#27)

--- src/cursor-plugin.ts
+++ src/cursor-plugin.ts
@@ -282,7 +282,9 @@ function absolutePositionToCursor(
   const loroId = WEAK_NODE_TO_LORO_CONTAINER_MAPPING.get(nodeParent) ??
     getByValue(mapping, nodeParent);
   if (!loroId) {
-    console.error("Cannot find the loroNode");
+    if (nodeParent.nodeSize > 4) {
+      console.error("Cannot find the loroNode");
+    }
     return;
   }
 
